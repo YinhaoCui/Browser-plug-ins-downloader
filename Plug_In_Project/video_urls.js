aed7a563892a91ac5050b1296cef89c857e6da1d
YinhaoCui <126128464+YinhaoCui@users.noreply.github.com>
2023-10-25 10:02:36 -0400

    Add files via upload

---
+++ Plug_In_Project/video_urls.js
@@ -0,0 +1,31 @@
+var videoType = ["flv", "avi", "mkv", "mov", "mp4", "mpeg", "mpg","mts",
+  "mxf", "ogv", "vep", "vob", "webm", "wlmp", "wmv", "rm", "swf","jpg","png","jpeg","gif","mp3"];
+
+//convert webpage html to single string
+var htmlText = document.body.innerHTML.toString();
+
+var videoRegex = videoType.join("|");
+
+// pattern to match urls that point to videos
+//code adapted from  https://github.com/dimofte/chrome-video-downloader/blob/master/scrape_video_urls.js
+var urlRegex  = new RegExp('("|\')(http|https):\\/\\/[^("|\'|,)]*?\\.(' +videoRegex +')[^("|\')]*?("|\')','gi');
+
+
+// Build the array of video urls
+var urls = htmlText.match(urlRegex);
+// remove quotes from strings
+urls = urls.map(function(quotes) {
+      return quotes.substr(1, quotes.length-2)
+    });
+  
+var fileRegex = new RegExp('[^\/]+?\.(' + videoRegex + ')');
+    
+var vidData = urls.map(function(url) {
+  
+  return {
+    name: url.match(fileRegex)[0],
+    url: url
+  }
+});
+
+chrome.extension.sendRequest(vidData);
---
+++ README.md
@@ -0,0 +1,24 @@
+
+Description:A chrome plugin, the main function is to download videos in Web pages. At the same time, you can view the downloaded history in the plug-in.
+
+
+Types of vieos available for download: "flv", "avi", "mkv", "mov", "mp4", "mpeg", "mpg","mts",
+  "mxf", "ogv", "vep", "vob", "webm", "wlmp", "wmv", "rm", "swf"
+
+Instruction:
+
+1. Download the plug-in. 
+
+2. Apply the plug-in.
+
+3. The plug-in will be shown in the chrome tab.
+
+4. Go to the webpage which you want to explore.
+
+5. Click the download button to save files in your local machine.
+
+** The application is not available on Youtube at the moment. ** 
+
+
+If you have any questions, feel free to email via Minwoo.Yang@dal.ca 
+Enjoy our plug-in ! 
